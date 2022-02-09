d34d5b4f90d1f2cb8cf908ee1edf12fc8016949d
Leandro Ramos <leandroramos@Leandros-MacBook-Pro.local>
2022-02-08 21:01:08 -0300

    adding project

---
+++ 10-Project10/10-Project10/PersonCell.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
---
+++ 10-Project10/10-Project10/ViewController.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath)
+        return cell
+    }
+}
+
