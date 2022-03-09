e8b2650f5d3a357285779adebfb7b3fb515c4daf
leandroramosss <leandroaaramos@gmail.com>
2022-03-08 21:01:09 -0300

    change project code presentation

--- 10-Project10/10-Project10/PersonCell.swift
+++ 10-Project10/10-Project10/PersonCell.swift
@@ -4,4 +4,9 @@ class PersonCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var name: UILabel!
+
+    self.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+    self.imageView.layer.borderWidth = 2
+    self.imageView.layer.cornerRadius = 3
+    self.layer.cornerRadius = 7
 }
--- 10-Project10/10-Project10/ViewController.swift
+++ 10-Project10/10-Project10/ViewController.swift
@@ -1,78 +1,31 @@
 import UIKit
 
-class ViewController: UICollectionViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+class ViewController: UICollectionViewController {
 
     var people = [Person]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addNavigationItem()
+    }
+
+    private func addNavigationItem() {
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
                                                            target: self,
                                                            action: #selector(addNewPerson))
     }
-    
-    
-    
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return people.count
-    }
-    
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
-            fatalError("unable to dequeue person cell")
-        }
-        
-        //because is a grid it has items instead
-        let person = people[indexPath.item]
-        
-        cell.name.text = person.name
-        
-        let path = getDocumentDirectory().appendingPathComponent(person.image)
-        
-        cell.imageView.image = UIImage(contentsOfFile: path.path)
-        
-        cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
-        cell.imageView.layer.borderWidth = 2
-        cell.imageView.layer.cornerRadius = 3
-        cell.layer.cornerRadius = 7
-        
-        return cell
-    }
-    
-    @objc func addNewPerson() {
+
+    @objc private func addNewPerson() {
         let picker = UIImagePickerController()
         picker.allowsEditing = true
         picker.delegate = self
         present(picker, animated: true)
     }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        //read image
-        guard let image = info[.editedImage] as? UIImage else {
-            return
-        }
-        
-        let imageName = UUID().uuidString
-        let imagePath = getDocumentDirectory().appendingPathComponent(imageName)
-        
-        if let jpegData = image.jpegData(compressionQuality: 0.8) {
-            try? jpegData.write(to: imagePath)
-        }
-        
-        let person = Person(name: "Unknow", image: imageName)
-        people.append(person)
-        
-        collectionView.reloadData()
-        
-        dismiss(animated: true, completion: nil)
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return people.count
     }
-    
-    func getDocumentDirectory() -> URL {
-        let path = FileManager.default.urls(for: .documentDirectory,
-                                              in: .userDomainMask)
-        return path[0]
-    }
-    
+
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
         
@@ -92,3 +45,47 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     }
 }
 
+extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("unable to dequeue person cell")
+        }
+
+        //because is a grid it has items instead
+        let person = people[indexPath.item]
+
+        cell.name.text = person.name
+
+        let path = getDocumentDirectory().appendingPathComponent(person.image)
+
+        cell.imageView.image = UIImage(contentsOfFile: path.path)
+        return cell
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //read image
+        guard let image = info[.editedImage] as? UIImage else {
+            return
+        }
+
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentDirectory().appendingPathComponent(imageName)
+
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+
+        let person = Person(name: "Unknow", image: imageName)
+        people.append(person)
+
+        collectionView.reloadData()
+
+        dismiss(animated: true, completion: nil)
+    }
+
+    func getDocumentDirectory() -> URL {
+        let path = FileManager.default.urls(for: .documentDirectory,
+                                              in: .userDomainMask)
+        return path[0]
+    }
+}
